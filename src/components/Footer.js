0c67a2bead35c3a7e26caf06878638f66b287163
souravshetty <sourav.s.shetty@gmail.com>
2025-09-12 14:04:48 -0700

    React using CRA and basic flow

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+		<div>
+			<div>TestYnatra private Limited </div>
+            <p>Email:Sourav</p>
+            <ul>
+                <li>@Test</li>
+
+            </ul> 
+		</div>
+	);
+}
+
+export default Footer
---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Heading() {
+  return (
+		<div>
+			<div>this is a Heding</div>
+			<div>THis is sourav</div>
+		</div>
+	);
+}
+
+export default Heading
